8d88018d3069538b00ab5afa975c665a82274fc5
James Overmoyer <jpo5322@psu.edu>
2020-09-09 15:08:02 +0000

    added new lab2-c with arrays to simplify the problem

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 // Author: James Overmoyer jpo5322@psu.edu
+// Collaborator:
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <readline/readline.h>
 
 int main(void) {
-  char *name = readline("Enter your name: ");
-  printf("Hello, %s!\n", name);
+  double gradePercentage[8] = {93, 90, 87, 83, 80, 77, 70, 60};
+  char *gradeLetters[8] = {"A", "A-", "B+","B","B-","C+","C","D"};
+  char *grade = "F";
+
+  double number = atof(readline("Enter your CMPSC 131 grade: "));
+  
+  for(int c = 0; c<=7;c++){
+    if(number >= gradePercentage[c]){
+      grade = gradeLetters[c];
+      break;
+    }
+  }
+  
+  printf("Your letter grade for CMPSC 131 is %s.\n", grade);
   return 0;
 }
